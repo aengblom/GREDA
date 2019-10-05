98db50eff01405c41a22c57701df98efe627b36d
Andrea Edstrom <aedstrom@mpr.org>
2019-10-05 18:14:01 -0500

    add dashboard styles

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React from "react";
 import "./App.css";
-//import "./Components/Home.js"
-//import "./Components/RecycleMe.js"
-//import "./Components/ShareMe.js"
-//import "./Components/TeachMe.js"
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
-import Card from "./Components/Card";
 
 export default function App() {
   return (
     <div classWrapper="app">
       <Header></Header>
-      <div className="wrapper">
-        <Card></Card>
-      </div>
+
       <Footer></Footer>
     </div>
   );
--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -2,14 +2,11 @@ import React from "react";
 import "./Card.css";
 import classnames from "classnames";
 
-const Card = ({ children, className }) => (
-  <div className="card">
-    <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">
-        Some quick example text to build on the card title and make up the bulk
-        of the card's content.
-      </p>
+const Card = props => (
+  <div className="card card-extraPadding card-extraMargin card-shadow">
+    <div className="card-body card-extraPadding text-center">
+      <h2 className="card-title h1">{props.cardtext}</h2>
+      <input></input>
     </div>
   </div>
 );
