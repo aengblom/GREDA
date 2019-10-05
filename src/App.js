c21c5a645f41453cf71aa74c081eededaec4ff74
Vger <vcdelariva@gmail.com>
2019-10-05 16:31:21 -0500

    Move router to header

--- src/App.js
+++ src/App.js
@@ -9,64 +9,11 @@ import Header from "./Components/Header";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Footer from "./Components/Footer";
 
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function RecycleMe() {
-  return <h2>Recycle Me!</h2>;
-}
-
-function Share() {
-  return <h2>Share</h2>;
-}
-
-function Teach() {
-  return <h2>Teach Me</h2>;
-}
-
 export default function App() {
   return (
     <div>
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/recycle">Recycle Me</Link>
-            </li>
-            <li>
-              <Link to="/share">Share Me</Link>
-            </li>
-            <li>
-              <Link to="/teach">Teach Me</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/recycle">
-            <RecycleMe />
-          </Route>
-          <Route path="/share">
-            <Share />
-          </Route>
-          <Route path="/teach">
-            <Teach />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-    
-    <Footer></Footer>
+      <Header></Header>
+      <Footer></Footer>
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,55 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./Header.css";
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function RecycleMe() {
+  return <h2>Recycle Me!</h2>;
+}
+
+function Share() {
+  return <h2>Share</h2>;
+}
+
+function Teach() {
+  return <h2>Teach Me</h2>;
+}
 
 class Header extends Component {
   render() {
-    return <div>Test</div>;
+    return (
+      <div>
+        <Router>
+          <nav className="navbar navbar-expand-lg navbar-dark grad">
+            <a className="navbar-brand" href="#">
+              GREDA
+            </a>
+            <Link to="/">Home</Link>
+            <Link to="/recycle">Recycle Me</Link>
+            <Link to="/share">Share Me</Link>
+            <Link to="/teach">Teach Me</Link>
+          </nav>
+          <Switch>
+            <Route path="/recycle">
+              <RecycleMe />
+            </Route>
+            <Route path="/share">
+              <Share />
+            </Route>
+            <Route path="/teach">
+              <Teach />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    );
   }
 }
 
