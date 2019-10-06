b9c430559d0e5195e793f3cad35efbaaca3ec232
Vger <vcdelariva@gmail.com>
2019-10-05 20:16:34 -0500

    Pass props to Location

--- src/Components/Location/Location.js
+++ src/Components/Location/Location.js
@@ -1,12 +1,14 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from "react";
+import "./Location.css";
 
 class Location extends Component {
   render() {
     return (
-      <div>
-        <h1>St. Paul</h1>
-        <h2>Minnesota</h2>
-        <h4>change</h4>
+      <div id="location" className="p-2 flex-grow-1">
+        <p className="city">{this.props.city}</p>
+        <p className="state">{this.props.state}</p>
+        <p className="change">change</p>
       </div>
     );
   }
