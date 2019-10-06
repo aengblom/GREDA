41a255dc767e4a3366ec228cb21ab0a774cd8eb5
Andrea Engblom <andrea@creedinteractive.com>
2019-10-05 21:16:37 -0500

    Putting in right booleans

--- src/Components/ChildPage/ChildPage.js
+++ src/Components/ChildPage/ChildPage.js
@@ -13,22 +13,22 @@ class ChildPage extends Component {
                         <TeaserBubble
                             src="http://via.placeholder.com/100x100"
                             to=''
-                            icon = "true"
-                            success = "true"
+                            icon = {true}
+                            success = {true}
                             title="Yougurt, Butter, Margarine"
                         />
                         <TeaserBubble
                             src="http://via.placeholder.com/100x100"
                             to=''
-                            icon = "true"
-                            success = "true"
+                            icon = {true}
+                            success = {true}
                             title="Transparent Clamshell Containers"
                         />
                         <TeaserBubble
                             src="http://via.placeholder.com/100x100"
                             to=''
-                            icon = "true"
-                            success = "false"
+                            icon = {true}
+                            success = {false}
                             title="Produce Containers"
                         />
                     </div>
@@ -37,22 +37,22 @@ class ChildPage extends Component {
                         <TeaserBubble
                             src="http://via.placeholder.com/100x100"
                             to=''
-                            icon = "true"
-                            success = "true"
+                            icon = {true}
+                            success = {false}
                             title="Plasic Bags or Film"
                         />
                         <TeaserBubble
                             src="http://via.placeholder.com/100x100"
                             to=''
-                            icon = "true"
-                            success = "true"
+                            icon = {true}
+                            success = {false}
                             title="Bottles Used for Home Needle Disposal"
                         />
                         <TeaserBubble
                             src="http://via.placeholder.com/100x100"
                             to=''
-                            icon = "true"
-                            success = "false"
+                            icon = {true}
+                            success = {false}
                             title="Containers Larger Than 3 Gallons"
                         />
                     </div>
@@ -62,22 +62,22 @@ class ChildPage extends Component {
                         <TeaserBubble
                             src="http://via.placeholder.com/100x100"
                             to=''
-                            icon = "true"
-                            success = "true"
+                            icon = {true}
+                            success = {false}
                             title="Containers That Held Hazardous Materials"
                         />
                         <TeaserBubble
                             src="http://via.placeholder.com/100x100"
                             to=''
-                            icon = "true"
-                            success = "true"
+                            icon = {true}
+                            success = {false}
                             title="Medicine Bottles"
                         />
                         <TeaserBubble
                             src="http://via.placeholder.com/100x100"
                             to=''
-                            icon = "true"
-                            success = "false"
+                            icon = {true}
+                            success = {false}
                             title="Toy or Electronic Packaging"
                         />
                     </div>
