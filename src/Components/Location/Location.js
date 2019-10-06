ce739830746999f0ac41af4fb493a6c6c3a5529d
Vger <vcdelariva@gmail.com>
2019-10-05 19:10:27 -0500

    Add Location component

---
+++ src/Components/Location/Location.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Location extends Component {
+  render() {
+    return (
+      <div>
+        <h1>St. Paul</h1>
+        <h2>Minnesota</h2>
+        <h4>change</h4>
+      </div>
+    );
+  }
+}
+
+export default Location;
