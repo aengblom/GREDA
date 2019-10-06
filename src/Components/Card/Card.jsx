e7c18f95784a979492dfaafa0afed6689d2001c3
Jayna <jayna@Jaynas-MacBook-Pro-2.local>
2019-10-06 11:23:02 -0500

    changed list styles

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -7,7 +7,7 @@ const Card = props => {
   return (
     <div className="card card-extraPadding card-extraMargin card-shadow">
       <div className="card-body card-extraPadding text-center">
-        <h2 className="card-title h1 h1-em">{props.cardtext}</h2>
+        <h2 className="card-title h2 h2-em">{props.cardtext}</h2>
         {props.noInput || <ZipCode setShowSP={props.setShowSP}></ZipCode>}
       </div>
     </div>
